4a327a2a33774fdfba988d428d32078fec15bef7
liav2002 <liavmail00@gmail.com>
2021-10-20 10:20:27 +0300

    create new branch for developing. create project in vs.

---
+++ Medical System/Medical System/main.cpp
@@ -0,0 +1,13 @@
+/*
+* Describe: Computerized system for call center management medical. 
+* Name of develepor 1: Eyal Zecbah. ID: 324863539.
+* Name of develepor 2: Liav Ariel.  ID: 212830871.
+*/
+
+#include <iostream>
+
+int main()
+{
+
+	return 0;
+}
