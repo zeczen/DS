a5627167f77f013207acc1c4e1495f5120b068a9
liav2002 <liavmail00@gmail.com>
2021-10-20 14:31:01 +0300

    Implemnted decision-tree.

--- Medical System/Medical System/main.cpp
+++ Medical System/Medical System/main.cpp
@@ -15,9 +15,10 @@ using std::endl;
 
 int main()
 {
-	DecisionTreeNode t;
+	DT t;
 	string father, val, sol;
 	char ch;
+
 	cout << "\nDECISION TREE\n";
 	cout << "Choose one of the following:" << endl;
 	cout << "n: New decision tree" << endl;
@@ -28,23 +29,95 @@ int main()
 	cout << "r: Run a process of questions" << endl;
 	cout << "t: Print to a string" << endl;
 	cout << "e: exit:" << endl;
+
 	do
 	{
 		cin >> ch;
 		switch (ch)
 		{
-		case 'n':cout << "enter the main question (with no space) "; cin >> val; t.addRoot(val); break;
-		case 's':cout << "enter the question (with no space) "; cin >> father;
-			cout << "enter the new answer (with no space) "; cin >> val;
-			cout << "enter the new solution(possible a new question, with no space) "; cin >> sol;
-			if (t.addSon(father, val, sol)) cout << "success\n"; else cout << "ERROR\n"; break;
-		case 'd':cout << "enter value of subtree to delete (with no space) "; cin >> val; t.deleteSubTree(val);  break;
-		case 'p':t.printAllTree();  break;
-		case 'w':cout << "enter a value (with no space) "; cin >> val; t.searchAndPrint(val); cout << endl;  break;
-		case 'r':t.process(); cout << endl;  break;
-		case 'e':cout << "bye "; break;
-		case 't':cout << t.printToString(); cout << endl;  break;
-		default: cout << "ERROR\n";  break;
+			case 'n':
+			{
+				cout << "enter the main question (with no space) "; 
+				cin >> val; 
+				t = DT(val); 
+				break;
+			}
+
+			case 's':
+			{
+				cout << "enter the question (with no space) "; 
+				cin >> father;
+			
+				cout << "enter the new answer (with no space) "; 
+				cin >> val;
+			
+				cout << "enter the new solution(possible a new question, with no space) "; 
+				cin >> sol;
+
+				if (t.addNode(father, val, sol))
+				{
+					cout << "success\n";
+				}
+
+				else
+				{
+					cout << "ERROR\n";
+				}
+
+				break;
+			}
+
+			case 'd':
+			{
+				cout << "enter value of subtree to delete (with no space) "; 
+				cin >> val; 
+				t.removeSubTree(val);  
+				break;
+			}
+
+			case 'p':
+			{
+				t.print();  
+				break;
+			}
+
+			case 'w':
+			{
+				cout << "enter a value (with no space) "; 
+				cin >> val; 
+				t.printPath(val); 
+				cout << endl;  
+				break;
+			}
+
+			case 'r':
+			{
+				t.run(); 
+				cout << endl;  
+				break;
+			}
+
+			case 'e':
+			{
+				cout << "bye ";
+				break;
+			}
+
+			case 't':
+			{
+				cout << t.printToString(); 
+				cout << endl;  
+				break;
+			}
+
+			default: 
+			{
+				cout << "ERROR\n";  
+				break;
+			}
 		}
+
 	} while (ch != 'e');
+
+	return 0;
 }
