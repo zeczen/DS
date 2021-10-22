daa1d71893c56320c426b323181f19159c7e8e05
Eyal <54035934+zeczen@users.noreply.github.com>
2021-10-22 09:47:43 +0300

    Update main.cpp

--- Medical System/Medical System/main.cpp
+++ Medical System/Medical System/main.cpp
@@ -1,6 +1,6 @@
 /*
 * Describe: Computerized system for call center management medical. 
-* Name of develepor 1: Eyal Zecbah. ID: 324863539.
+* Name of develepor 1: Eyal Seckbach. ID: 324863539.
 * Name of develepor 2: Liav Ariel.  ID: 212830871.
 */
 
