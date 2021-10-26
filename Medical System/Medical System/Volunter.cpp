efddd1bf75536aed740918d98e465548c2693b4f
Eyal <seyal613@gmail.com>
2021-10-26 22:56:17 +0300

    create volunter class

---
+++ Medical System/Medical System/Volunter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by seyal on 26/10/2021.
+//
+
+#include "Volunter.h"
---
+++ Medical System/Medical System/Volunter.h
@@ -0,0 +1,11 @@
+
+#ifndef DS_VOLUNTER_H
+#define DS_VOLUNTER_H
+
+
+class Volunter {
+
+};
+
+
+#endif //DS_VOLUNTER_H
