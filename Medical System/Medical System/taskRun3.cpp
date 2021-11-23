48716a85e6528aa0af86c3bc6c901c0b9de15741
liav2002 <liavmail00@gmail.com>
2021-11-23 19:07:43 +0200

    Start task 3.

---
+++ Medical System/Medical System/taskRun3.cpp
@@ -0,0 +1,11 @@
+/*
+* Describe: Computerized system for call center management medical.
+* Name of developer 1: Eyal Seckbach. ID: 324863539.
+* Name of developer 2: Liav Ariel. ID: 212830871.
+*/
+
+int main()
+{
+
+	return 0;
+}
