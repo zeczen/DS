f050e858caaf63891e22118ea5dd781f82b385eb
Your github username <seyal613@gmail.com>
2021-12-16 16:05:00 +0200

    try to fix the issue, not successfully

--- Medical System/Medical System/Huffman.cpp
+++ Medical System/Medical System/Huffman.cpp
@@ -18,10 +18,10 @@ BinaryTree<Pair> &HuffmanTree::buildHuffman(std::string word) {
      * if type of <BinaryTree<Pair*> *> the '<' and '>' operators not called on BinaryTree type but on the address of them in the memory
      * !!!
      */
+    int i = 0;
     PQ::updatable_priority_queue<int, BinaryTree<Pair>> huffmanPQ; // priority queue declaration
     HashInt *hashInt;
     hashInt = new HashInt(word.length());
-    // huffmanPQ = new std::priority_queue<BinaryTree<Pair*> >();
     for (char &c: word) {
         if (hashInt->search(c) == -1)
             // the first occurrence of the current character
@@ -29,20 +29,19 @@ BinaryTree<Pair> &HuffmanTree::buildHuffman(std::string word) {
             int count = getCommon(word, c);
             hashInt->insert(new Item<int, int>(count, c));
             // the key is the char and the value is the amount
-            const BinaryTree<Pair> node = BinaryTree<Pair>(Pair(c, count));
-            huffmanPQ.push(count, node);
+            huffmanPQ.push(i++, BinaryTree<Pair>(Pair(c, count)));
             std::cout << huffmanPQ.size() << std::endl;
 
         }
     }
 
     while (huffmanPQ.size() >= 2) {
-        BinaryTree<Pair> max1 = huffmanPQ.pop_value().priority;
-        BinaryTree<Pair> max2 = huffmanPQ.pop_value().priority;
+        auto max1 = huffmanPQ.pop_value();
+        auto max2 = huffmanPQ.pop_value();
 
 
-        int sum = max1.getRoot().count + max2.getRoot().count;
-        huffmanPQ.push(sum, BinaryTree<Pair>(Pair(sum)));
+        int sum = max1.priority.getRoot().count + max2.priority.getRoot().count;
+        huffmanPQ.push(i++, BinaryTree<Pair>(Pair(sum)), sum);
 
     }
 
