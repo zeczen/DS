fe877b0115622e5f5952bb6b1997e34817c3cd98
Eyal <seyal613@gmail.com>
2022-02-23 13:59:58 +0200

    initial commit

--- taskRun1.cpp
+++ taskRun1.cpp
@@ -1,9 +1,3 @@
-/*
-* Describe: Computerized system for call center management medical.
-* Name of develepor 1: Eyal Seckbach. ID: 324863539.
-* Name of develepor 2: Liav Ariel.  ID: 212830871.
-*/
-
 #include <iostream>
 #include <list>
 #include <string>
--- taskRun5.cpp
+++ taskRun5.cpp
@@ -1,9 +1,3 @@
-/*
-* Describe: Computerized system for call center management medical.
-* Name of developer 1: Eyal Seckbach. ID: 324863539.
-* Name of developer 2: Liav Ariel. ID: 212830871.
-*/
-
 #include <iostream>
 #include "Graph.h"
 
